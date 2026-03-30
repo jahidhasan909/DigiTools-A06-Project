7bc7dd2ec7fe3bc31a535be1c611d9998b57c030
Jahid hasan <jahidhasan@Jahids-MacBook-Air.local>
2026-03-30 19:35:46 +0600

    button toggle compeleted

--- src/Components/CardSection.jsx
+++ src/Components/CardSection.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CardSection = () => {
+
+
+    const [selectType, setSelectType] = useState('products')
+
     return (
-        <div className='mt-20'>
-            <h1 className='text-center'>Premium Digital Tools</h1>
-            <p className='text-center'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
+        <div className='mt-20 space-y-3'>
+            <h1 className='text-center font-bold text-4xl'>Premium Digital Tools</h1>
+            <p className='text-center text-neutral/50'>Choose from our curated collection of premium digital products designed <br /> to boost your productivity and creativity.</p>
             <div className='flex justify-center items-center'>
-                <button className='btn'>Products</button>
-                <button className='btn'>Cart (2)</button>
+                <button onClick={() => setSelectType('products')} className={`btn rounded-3xl  ${selectType === 'products' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white' : ''}`}>Products</button>
+                <button onClick={() => setSelectType('Cart')} className={`
+                ${selectType === 'Cart' ? 'bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white' : ''}
+                    btn rounded-3xl `}>Cart (2)</button>
             </div>
             <div className='bg-gray-500 py-80'>
 
